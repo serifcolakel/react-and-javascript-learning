98d31985bed337c4e896b4f9116f77d825d78881
ŞERİF ÇOLAKEL <84101614+serifcolakel@users.noreply.github.com>
2021-10-11 23:33:32 +0300

    Promise olusturuldu ve app.js promiseye gore set edildi

--- callback-funtions/app.js
+++ callback-funtions/app.js
@@ -1,30 +1,48 @@
 console.log("START");
 
-function loginUser(email, password, callBack) {
-  setTimeout(() => {
-    console.log("now we have the data");
-    callBack({ userEmail: email });
-  }, 5000);
+function loginUser(email, password) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("now we have the data");
+      resolve({ userEmail: email });
+    }, 5000);
+  });
 }
 
-function getUserVideos(email, callback) {
-  setTimeout(() => {
-    callback(["video1", "video2", "video3"]);
-  }, 1000);
+function getUserVideos(email) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(["video1", "video2", "video3"]);
+    }, 1000);
+  });
 }
-function videoDetail(video, callback) {
-  setTimeout(() => {
-    callback("title of the video");
-  }, 2000);
+function videoDetails(video, callback) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("title of the video");
+    }, 2000);
+  });
 }
+
+/* UGLY
 const user = loginUser("serifc@gmail.com", 1235125, (user) => {
   console.log(user);
+  //CALLBACK structure
   getUserVideos(user.userEmail, (videos) => {
     console.log(videos);
-    videoDetail(videos[0], (title) => {
+    videoDetails(videos[0], (title) => {
       console.log(title);
     });
   });
 });
+*/
+
+/*Simply UGLY -> BEAuty */
+loginUser("serif", "colakel")
+  .then((user) => getUserVideos(user.email))
+  .then((videos) => videoDetails(videos[0]))
+  .then((detail) => console.log(detail));
+
+//Sync Code
 
 console.log("END");
--- callback-funtions/promise.js
+++ callback-funtions/promise.js
@@ -1,1 +1,15 @@
-console.log("promise");
+//Basitçe Promise Aciklamasi
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("got the user ");
+    reject(new Error("User not logged in"));
+    // resolve({ user: "serif" });
+  }, 2000);
+});
+
+//.then meaning = we have the information
+promise
+  .then((user) => {
+    console.log(user);
+  })
+  .catch((err) => console.log(err.message));
